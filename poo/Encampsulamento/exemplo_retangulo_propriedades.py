6b30b7c433187fff2b6f175c8b5e9a37b227ff92
joao-nzuzi <joaonzuzi21@gmail.com>
2024-07-10 17:21:20 +0100

    Exemplo de getter, setter e deleter usando property

---
+++ poo/Encampsulamento/exemplo_retangulo_propriedades.py
@@ -0,0 +1,35 @@
+class Retangulo:
+    def __init__(self, largura, altura):
+        self.largura = largura
+        self.altura = altura
+        
+    @property
+    def largura(self):
+        return self._largura
+    
+    @largura.setter
+    def largura(self, nova_largura):
+        if nova_largura > 0:
+            self._largura = nova_largura
+        else:
+            raise ValueError("A largura deve ser maior que 0.")
+    
+    @largura.deleter
+    def largura(self):
+        del self._largura
+    
+    @property
+    def area(self):
+        return self.largura * self.altura
+
+retangulo = Retangulo(5, 3)
+print(retangulo.largura)  
+
+retangulo.largura = 7
+print(retangulo.largura)  
+
+retangulo.largura = -1  
+
+del retangulo.largura
+
+print(retangulo.largura)
